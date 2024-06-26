14e7f20b4746c79b1f8af1322d5fce4cdc95362c
guiduzera <guidu.dev@gmail.com>
2024-06-25 23:49:59 -0300

    FEAT: Colocando middleware para mudar o idioma da api no ar e adicionando filtro de linguas suportadas

---
+++ src/CashFlow.API/Middlewares/CultureMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace CashFlow.API.Middlewares;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
+
+        var requestedLangauage = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+        var defaultCulture = new CultureInfo("pt-BR");
+
+        if (string.IsNullOrWhiteSpace(requestedLangauage) == false && supportedLanguages.Exists((e) => e.Name.Equals(requestedLangauage)))
+        {
+            defaultCulture = new CultureInfo(requestedLangauage);
+        }
+
+        CultureInfo.CurrentCulture = defaultCulture;
+        CultureInfo.CurrentUICulture = defaultCulture;
+
+        await _next(context);
+    }
+}
