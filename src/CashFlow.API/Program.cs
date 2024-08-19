bdada1094bba8f59210e610330fe4a46b3cfd770
guiduzera <guidu.dev@gmail.com>
2024-08-19 11:01:42 -0300

    FEAT: Adicionando health checks para a api

--- src/CashFlow.API/Program.cs
+++ src/CashFlow.API/Program.cs
@@ -1,5 +1,8 @@
 using CashFlow.API.Filters;
 using CashFlow.API.Middlewares;
+using CashFlow.Infrastructure.DataAccess;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +16,20 @@ builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 
+builder.Services.AddHealthChecks().AddDbContextCheck<CashFlowDbContext>();
+
 var app = builder.Build();
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    AllowCachingResponses = false,
+    ResultStatusCodes =
+    {
+        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable,
+        [HealthStatus.Degraded] = StatusCodes.Status500InternalServerError,
+    }
+});
 
 if (app.Environment.IsDevelopment())
 {
